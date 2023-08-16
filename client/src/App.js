d165a60b1b166cfc6f8c5f6e031833fe1aff8469
SethSchnrt <Seth.Schoenert@gmail.com>
2023-08-16 18:18:05 -0500

    fixed apollo linked context package that was causing errors

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
