208c65fbb0dd8929aa171f7e30d68ce386f03dd4
SethSchnrt <Seth.Schoenert@gmail.com>
2023-08-17 16:03:17 -0500

    fixed another server.js syntax error

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const { ApolloServer } = require('apollo/server-express');
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./auth');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
