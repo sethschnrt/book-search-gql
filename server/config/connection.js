25b113a4d82a86fd99b3f6810ad47ba085b62c04
SethSchnrt <Seth.Schoenert@gmail.com>
2023-08-18 23:08:57 -0500

    fixed issues with connection and savedbooks

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
 
 module.exports = mongoose.connection;
